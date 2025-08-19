2c6c3fe580d2d0a42540a525aa21ed2cc0a59e4f
Bryan Behrenshausen <bryan.behrenshausen@sas.com>
2025-08-19 10:58:29 -0400

    Update docusaurus.config.ts to change doc production URL
    Signed-off-by: Bryan Behrenshausen <bryan.behrenshausen@sas.com>

--- website/docusaurus.config.ts
+++ website/docusaurus.config.ts
@@ -8,7 +8,7 @@ const config: Config = {
   favicon: "images/sas.png",
 
   // Set the production url of your site here
-  url: "https://miniature-bassoon-6kry92e.pages.github.io",
+  url: "https://sassoftware.github.io",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: "/sas-trustworthy-ai-life-cycle/",
